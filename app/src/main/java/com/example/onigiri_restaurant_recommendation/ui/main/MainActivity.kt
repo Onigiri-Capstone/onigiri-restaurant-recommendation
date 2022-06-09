c21aaa0ff5b5a6d26431aa54752dddae7f4b5441
alpian-12 <99374963+alpian-12@users.noreply.github.com>
2022-06-09 09:54:56 +0700

    Revert "Revert "sync project""

--- app/src/main/java/com/example/onigiri_restaurant_recommendation/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/onigiri_restaurant_recommendation/ui/main/MainActivity.kt
@@ -1,37 +1,24 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.onigiri_restaurant_recommendation.ui.main
 
-import android.Manifest
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.onigiri_restaurant_recommendation.R
 import com.example.onigiri_restaurant_recommendation.databinding.ActivityMainBinding
-import com.example.onigiri_restaurant_recommendation.ui.bottomsheet.NoInternetBottomSheet
-import com.example.onigiri_restaurant_recommendation.util.*
+import com.example.onigiri_restaurant_recommendation.util.callNetworkConnection
+import com.example.onigiri_restaurant_recommendation.util.location.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var checkGPS: CheckGPS
-
-    companion object {
-        const val LOCATION_RESULT = 200
-
-        private val REQUIRED_PERMISSIONS
-                = arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        )
-
-        private const val REQUEST_CODE_PERMISSIONS = 100
-    }
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var gpsStatus: GpsStatus
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,37 +26,49 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        checkGPS = CheckGPS(this,this, supportFragmentManager)
-        checkGPS.getUserLocation()
-
         callNetworkConnection(application, this, supportFragmentManager)
+
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        subscribeToLocationPermissionListener()
+        subscribeToGpsListener()
 
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navView.setupWithNavController(navController)
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (checkPermission(REQUIRED_PERMISSIONS, this@MainActivity)) {
-                if(!isLocationEnabled(this@MainActivity)) {
-                    requestGPS(supportFragmentManager)
-                }
-            } else {
-                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                    data = Uri.fromParts("package", packageName, null)
-                }
-                startActivity(intent)
+    private fun subscribeToGpsListener() = mainViewModel.gpsStatusLiveData
+        .observe(this, gpsObserver)
+
+
+    private val gpsObserver = Observer<GpsStatus> { status ->
+        status?.let {
+            gpsStatus = status
+            updateGpsCheckUI(status)
+        }
+    }
+
+    private fun updateGpsCheckUI(status: GpsStatus) {
+        when (status) {
+            is GpsStatus.Disabled -> {
+                showGpsPermission(supportFragmentManager)
+            }
+            else -> {}
+        }
+    }
+
+    private fun subscribeToLocationPermissionListener() =
+        mainViewModel.locationPermissionStatusLiveData.observe(this, permissionObserver)
+
+    private val permissionObserver = Observer<PermissionStatus> { status ->
+        status?.let {
+            when (status) {
+                is PermissionStatus.Denied -> requestLocationPermission(this)
             }
         }
     }
 
     override fun onBackPressed() {
-        finish()
+        finishAffinity()
     }
 }
