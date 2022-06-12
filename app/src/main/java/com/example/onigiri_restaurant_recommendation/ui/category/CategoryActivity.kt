ebf4eb6830244f1dfba1a1b62207f9e875df8033
alpiansyah1204 <96229888+alpiansyah1204@users.noreply.github.com>
2022-06-12 23:56:09 +0700

    dark mode revisi

--- app/src/main/java/com/example/onigiri_restaurant_recommendation/ui/category/CategoryActivity.kt
+++ app/src/main/java/com/example/onigiri_restaurant_recommendation/ui/category/CategoryActivity.kt
@@ -189,7 +189,7 @@ class CategoryActivity : AppCompatActivity() {
                             IntentSenderRequest.Builder(exception.resolution).build()
                         )
                     } catch (sendEx: IntentSender.SendIntentException) {
-                        Toast.makeText(this, sendEx.message, Toast.LENGTH_SHORT).show()
+//                        Toast.makeText(this, sendEx.message, Toast.LENGTH_SHORT).show()
                     }
                 }
             }
